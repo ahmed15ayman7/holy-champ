e8880cd3198aa26abbfbd84e11582aa02dfacd75
Ziad Eldeep <ziaeldeep84@gmail.com>
2024-12-01 22:33:02 +0200

    first1234111

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,19 @@
-import { SignIn } from '@clerk/nextjs'
+import { SignIn } from '@clerk/nextjs';
 
 export default function Page() {
-  return <SignIn />
+  return (
+    <div style={styles.container}>
+        <SignIn />
+      </div>
+  );
 }
+
+const styles = {
+  container: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh', // Full viewport height
+    backgroundColor: '#f5f5f5', // Optional: Background color
+  }
+};
--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,19 @@
-import { SignUp } from '@clerk/nextjs'
+import { SignUp } from '@clerk/nextjs';
 
 export default function Page() {
-  return <SignUp />
+  return (
+    <div style={styles.container}>
+        <SignUp />
+      </div>
+  );
 }
+
+const styles = {
+  container: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh', // Full viewport height
+    backgroundColor: '#f5f5f5', // Optional background color
+  }
+};
